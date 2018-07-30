193beef74f97e335c7175172c9bf9bfaa336d711
vietpn <vietpnk53@gmail.com>
2018-07-30 17:39:25 +0700

    update

--- src/inject.js
+++ src/inject.js
@@ -1,11 +1,4 @@
-window.onload = () => {
-    const $ = require('jquery.js');
-    console.log($('#result_box'));
-}
-
-// let translate = () => {
-//     return document.getElementById("result_box").textContent;
-// }
-
-// console.log(translate());
-
+window.addEventListener('load', (ev) => {
+    $ = require('jquery');
+    console.log($('#result_box').text());
+})
